e2a209682fad30e8f2efb5fee273ee0afae6c3b3
Ruben Suardi <rubensuardi@Rubens-MacBook-Pro.local>
2017-04-06 10:16:12 -0400

    showing some data in the client from a variable value and using angular directives

--- wall/urls.py
+++ wall/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from .api import PostApi
 
 
 urlpatterns = [
     url(r'^posts$', PostApi.as_view()),
+    url(r'^home', TemplateView.as_view(template_name='wall/home.html'))
 
 ]
